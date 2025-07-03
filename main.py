1c3f0727a45f9e59f1a7d79d9830695cf0014616
Yug2210 <yugrohilla33@gmail.com>
2025-07-03 20:12:26 +0530

    Fix: listen on 0.0.0.0 and use Render port

--- main.py
+++ main.py
@@ -23,4 +23,6 @@ def webhook():
     return jsonify({{"fulfillmentText": answer}})
 
 if __name__ == '__main__':
-    app.run()
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
