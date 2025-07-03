eafb5761851f223a5f77fb60c6ac028e1554aaa3
Yug2210 <yugrohilla33@gmail.com>
2025-07-03 19:56:49 +0530

    Initial webhook code

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import sympy as sp
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Webhook is live!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    user_text = req.get('queryResult', {}).get('queryText', '')
+
+    try:
+        x = sp.symbols('x')
+        eq = sp.sympify(user_text.replace("=", "-(") + ")")
+        solution = sp.solve(eq, x)
+        answer = f"The solution is: x = {{solution[0]}}" if solution else "No solution found."
+    except Exception:
+        answer = "Sorry, I couldn't understand or solve that equation."
+
+    return jsonify({{"fulfillmentText": answer}})
+
+if __name__ == '__main__':
+    app.run()
